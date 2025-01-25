b51c9eb6872680f12dab91a141b7d8bc18f07ae4
ASPETTAESPERA\marco <marco.mina999@gmail.com>
2025-01-25 17:06:27 +0200

    25/01b

---
+++ SitoLtb/Migrations/20250125110938_addCategoriaToPost.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SitoLtb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategoriaToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Tournaments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LinkBando = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkPreiscrizione = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournaments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.Id);
+                });
+        }
+    }
+}
---
+++ SitoLtb/ViewModels/CreateTournamentVM.cs
@@ -0,0 +1,14 @@
+﻿namespace SitoLtb.ViewModels
+{
+    public class CreateTournamentVM
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime Data { get; set; }
+
+        public string? LinkBando { get; set; }
+
+        public string? LinkPreiscrizione { get; set; }
+        public string? Url { get; set; }
+    }
+}
